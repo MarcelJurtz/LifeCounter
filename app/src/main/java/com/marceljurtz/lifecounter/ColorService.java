59c8b9c4efe51850d76d9cf03ac43a53b21badd3
MarcelJurtz <jurtzmarcel@gmail.com>
2016-11-17 12:06:17 +0100

    Added Energy-Savemode for AMOLED Displays

--- app/src/main/java/com/marceljurtz/lifecounter/ColorService.java
+++ app/src/main/java/com/marceljurtz/lifecounter/ColorService.java
@@ -3,7 +3,7 @@ package com.marceljurtz.lifecounter;
 import android.graphics.Color;
 
 /**
- * Created by marcel on 11.11.16.
+ * Verwaltung der Farbwerte
  */
 public class ColorService {
     public static final int black = Color.parseColor("#CCC2C0");
@@ -11,5 +11,7 @@ public class ColorService {
     public static final int green = Color.parseColor("#9BD3AE");
     public static final int red = Color.parseColor("#FAAA8F");
     public static final int white = Color.parseColor("#FFFCD6");
-    public static final int powerSafe = Color.parseColor("#FFFFFF");
+    public static final int powerSafe = Color.parseColor("#000000");
+    public static final int powerSafeTextcolor = Color.parseColor("#CCC2C0");
+    public static final int regularTextcolor = Color.parseColor("#161618");
 }
