bb43fc7d3c2ec7be28cb8b779f8bc9f7ba676e29
MarcelJurtz <jurtzmarcel@gmail.com>
2018-11-16 15:56:56 +0100

    Update formatting for about page

--- app/src/main/java/com/marceljurtz/lifecounter/views/About/AboutActivity.java
+++ app/src/main/java/com/marceljurtz/lifecounter/views/About/AboutActivity.java
@@ -3,16 +3,22 @@ package com.marceljurtz.lifecounter.views.About;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.marceljurtz.lifecounter.models.PreferenceManager;
 import com.marceljurtz.lifecounter.R;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity implements IAboutView {
 
@@ -25,7 +31,7 @@ public class AboutActivity extends AppCompatActivity implements IAboutView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        mainWebView = (WebView)findViewById(R.id.wvAbout);
+        //mainWebView = (WebView)findViewById(R.id.wvAbout);
 
         navigationView = (NavigationView)findViewById(R.id.navigationViewAbout);
 
@@ -59,8 +65,50 @@ public class AboutActivity extends AppCompatActivity implements IAboutView {
                 return true;
             }
         });
+        /*
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise with us");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.icon_small)
+                .addItem(new Element().setTitle("Version 6.2"))
+                .addItem(adsElement)
+                .addGroup("Connect with us")
+                .addEmail("feedback@mjurtz.com")
+                .addWebsite("http://www.mjurtz.com/")
+                .addTwitter("@MarcelJurtz")
+                .addPlayStore("https://play.google.com/store/apps/developer?id=Marcel+Jurtz")
+                .addGitHub("MarcelJurtz")
+                .addItem(getCopyRightsElement())
+                .addItem(getChangeLog())
+                .addItem(getLibsElement())
+                .create();
+
+        setContentView(aboutPage);*/
+    }
+/*
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        copyRightsElement.setTitle(getResources().getString(R.string.about_copyright_header));
+        copyRightsElement.setValue(getResources().getString(R.string.about_copyright_content));
+        return copyRightsElement;
     }
 
+    Element getLibsElement() {
+        Element licsElement = new Element();
+        licsElement.setTitle(getResources().getString(R.string.about_lics_header));
+        licsElement.setValue(getResources().getString(R.string.about_lics_content));
+        return licsElement;
+    }
+
+    Element getChangeLog() {
+        Element chElement = new Element();
+        chElement.setTitle(getResources().getString(R.string.about_changelog_header));
+        chElement.setValue(getResources().getString(R.string.about_changelog_content));
+        return chElement;
+    }
+*/
     @Override
     protected void onDestroy() {
         presenter.onDestroy();
@@ -81,7 +129,7 @@ public class AboutActivity extends AppCompatActivity implements IAboutView {
 
     @Override
     public void loadAboutPage(String url) {
-        mainWebView.loadUrl(url);
+        //mainWebView.loadUrl(url);
     }
 
     @Override
